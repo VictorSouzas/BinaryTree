2c2e684fe9c058ce40f1866299616b2271fdce00
Victor Souza <victor_souzas@yahoo.com.br>
2017-09-20 11:40:37 -0300

    add thread

---
+++ BinaryTreeThread.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+
+namespace BinaryTree
+{
+    public class BinaryTreeThread : AvlTree<Thread>
+    {
+        public override bool GreaterThan(Thread value, Thread temp)
+        {
+            return value.ManagedThreadId > temp.ManagedThreadId;
+        }
+
+        public override bool LesserThan(Thread value, Thread temp)
+        {
+            return value.ManagedThreadId < temp.ManagedThreadId;
+        }
+    }
+}
