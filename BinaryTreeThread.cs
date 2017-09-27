c71daa6f76a0d35bb87e7986eb87776598332ce6
Victor Souza <victor_souzas@yahoo.com.br>
2017-09-27 12:56:04 -0300

    Must override method Transverse to check if the thread is alive

--- BinaryTreeThread.cs
+++ BinaryTreeThread.cs
@@ -4,6 +4,11 @@ namespace BinaryTree
 {
     public class BinaryTreeThread : AvlTree<Thread>
     {
+        protected void Transverse(BinaryTreeNode<Thread> currentNode)
+        {
+            base.Transverse(currentNode);
+        }
+        
         public override bool GreaterThan(Thread value, Thread temp)
         {
             return value.ManagedThreadId > temp.ManagedThreadId;
